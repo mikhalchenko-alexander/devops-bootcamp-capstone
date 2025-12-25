4ac65f5c757fb0e8a0050dc5c7e31c92f34e4e86
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-12-25 14:05:14 +0200

    Add Maven project.

---
+++ project3/java-app/script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage(String tag) {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t ${tag} ."
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "docker push ${tag}"
+    }
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+}
+
+return this
