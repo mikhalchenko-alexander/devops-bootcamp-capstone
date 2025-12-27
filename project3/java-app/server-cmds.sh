974c10fcdd0e52cb7b991de847635f3739967515
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-12-27 14:20:19 +0200

    Fix file name.

--- project3/java-app/server-cmds.sh
+++ project3/java-app/server-cmds.sh
@@ -4,5 +4,5 @@ export IMAGE=$1
 export DOCKER_USER=$2
 export DOCKER_PWD=$3
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
-docker-compose -f docker-compose.yaml up --detach
+docker-compose -f docker-compose.yml up --detach
 echo "success"
