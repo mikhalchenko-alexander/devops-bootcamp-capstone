326f73ab8ed547fb514918d3d8067b673eaf84cb
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-12-27 15:39:39 +0200

    Use custom builder.

--- project3/java-app/script.groovy
+++ project3/java-app/script.groovy
@@ -6,6 +6,7 @@ def buildJar() {
 def buildImage(String tag) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker buildx use amd64builder"
         sh "docker buildx build --platform linux/amd64 -t ${tag} --load ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh "docker push ${tag}"
