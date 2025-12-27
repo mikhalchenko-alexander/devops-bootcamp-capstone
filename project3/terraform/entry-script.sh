b1e007323f6585e87aa7f0e0563629a714d79bae
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-12-27 16:01:54 +0200

    Update docker compose architecture.

--- project3/terraform/entry-script.sh
+++ project3/terraform/entry-script.sh
@@ -5,5 +5,5 @@ sudo usermod -aG docker ec2-user
 
 # install docker-compose
 
-sudo curl -SL "https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -SL "https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-armv7" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
